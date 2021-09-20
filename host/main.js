a0b3c773590ecf5be7f38a1514994c36810eb526
janhenrikejme <jan.ejme@gmail.com>
2021-09-20 21:21:31 +0200

    Update main.js

--- host/main.js
+++ host/main.js
@@ -1,3 +1,13 @@
 (function() {
+  	function loadScript(src, async) {
+      var e2 = document.createElement('script');
+      e2.src = src;
+      e2.async = async;
+      e2.type = 'text/javascript';
+
+      var s2 = document.getElementsByTagName('script')[0];
+      s2.parentNode.insertBefore(e2, s2);
+	}
+  loadScript("https://janhenrikejme.github.io/host/libs/require.js");
   console.log("born to be alive!");
 } ());
