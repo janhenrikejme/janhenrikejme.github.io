0b82df93466435022869e8c4ca4447bddfd8bb08
janhenrikejme <jan.ejme@gmail.com>
2021-09-20 21:31:25 +0200

    Update main.js

--- host/main.js
+++ host/main.js
@@ -1,5 +1,5 @@
 (function() {
-  	function loadScript(src, async) {
+  function loadScript(src, async) {
       var e2 = document.createElement('script');
       e2.src = src;
       e2.async = async;
@@ -7,7 +7,45 @@
 
       var s2 = document.getElementsByTagName('script')[0];
       s2.parentNode.insertBefore(e2, s2);
-	}
-  loadScript("https://janhenrikejme.github.io/host/libs/require.js");
+  }
+  loadScript("https://janhenrikejme.github.io/host/libs/require.js", false);
+  loadScript("https://janhenrikejme.github.io/host/libs/require_config.js", false);	
+  require(['bb_host_communicator'], function(communicator) {			
+	  console.log("jauda");
+            $("#main").html("running");
+              communicator.registerHandler("get", function(data, respond) {
+                respond({username: "testusers"});
+              });
+
+              communicator.registerHandler("showsidebar", function(data, respond) {
+                $("body").append("<iframe id='helpfrm' src='" + data.sidebarurl + "'></iframe>");
+                respond({status: "ok"});
+              });
+
+              communicator.registerHandler("element_is", function(data, respond) {
+                respond({result: $('[data-element-ref="' + data.elementRef + '"]').is(data.selector)});
+              });
+
+              communicator.registerHandler("on", function(data, respond) {
+                $(document).on(data.event, data.selector, function(ev) {
+                    respond(communicator.eventToObject(ev));
+                });
+              });
+
+              $("#launchtool").click(function() {
+                $("body").append("<iframe id='frm' src='frame.html'></iframe>");
+              });
+
+              $("#showhelp").click(function() {
+                if(communicator.canHandleAction("showhelp")) {
+                  communicator.handleAction("showhelp");
+                } else {
+                  alert("default handler");
+                }
+              });
+
+  });	
+	
+	
   console.log("born to be alive!");
 } ());
