51a9e6b5e5afbe5675156e37c5afc38a7787a3a0
janhenrikejme <jan.ejme@gmail.com>
2021-09-22 21:36:37 +0200

    sdds

--- host/main.js
+++ host/main.js
@@ -68,6 +68,8 @@ var onCommunicatorLoaded = function (iframeCommunicatorServer) {
     $.get(pluginPath + "config.json", function(pluginConfig) {
       pluginAccess[pluginPath] = pluginConfig.access;
       console.log(pluginConfig.access);
+      console.log("aaa");
+      console.log(pluginAccess);
       pluginConfig.scopes.forEach(function(scope) {
         if (getActiveScopes().filter(Set.prototype.has, new Set(scope.scope)).length > 0) {
           $("body").append("<iframe data-plugin-path='" + pluginPath + "' id='frm' src='" + pluginPath + scope.url + "'></iframe>");
