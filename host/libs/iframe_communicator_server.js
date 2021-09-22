4bb50abd8a96f066b6d8a10bddd7578f222bc5a6
janhenrikejme <jan.ejme@gmail.com>
2021-09-22 19:39:28 +0200

    dbg

--- host/libs/iframe_communicator_server.js
+++ host/libs/iframe_communicator_server.js
@@ -5,8 +5,22 @@ function init() {
       if (handlers[e.data.messageHandler] == undefined) return;
 
         console.log("*-*-*-*-*-*-*-*-*-*-*-*-*-*-2");
-        console.log(e);
+        console.log(e.source);
         console.log($(e.sourceElement));
+
+        var iframes = document.getElementsByTagName('IFRAME');
+        for (var i = 0, iframe, win; i < iframes.length; i++) {
+          iframe = iframes[i];
+      
+          // Cross-browser way to get iframe's window object
+          win = iframe.contentWindow || iframe.contentDocument.defaultView;
+      
+          // Comparison
+          console.log(iframe.src +
+            (win === event.source ? ' MATCHES.\n' : ' is not our IFrame.\n'))
+        }
+
+
 
       handlers[e.data.messageHandler](e.data, (response) => {
           if (e.source && !(e.source instanceof MessagePort) && !(e.source instanceof ServiceWorker)) {
