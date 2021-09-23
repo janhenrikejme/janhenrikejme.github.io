0b439c51470e4a691dd5080adef4bc74d88654a2
janhenrikejme <jan.ejme@gmail.com>
2021-09-23 16:28:34 +0200

    moving

--- plugin/libs/require_config.js
+++ plugin/libs/require_config.js
@@ -10,7 +10,7 @@ require.config({
   paths: {
     'jquery': './jquery-3.3.1.min',
     'iframe_communicator_client': './iframe_communicator_client',
-    'canvas_api': './canvas_api',
+    'canvas_api': 'https://eesy.github.io/shared/plugins/v1/libs/canvas_api.js',
     'resources': './resources',
   }
 });
